c2adecd82f2122c66d6d812e59d4b278739d4063
ta32 <26617779+ta32@users.noreply.github.com>
2023-03-02 21:21:59 +1100

    fix path

--- ff_iframe/js/background.js
+++ ff_iframe/js/background.js
@@ -3,7 +3,7 @@ console.log("test");
 var iframe = document.createElement('iframe');
 // using remote src works
 // iframe.src = 'https://connect.trezor.io/8/iframe.html';
-iframe.src = browser.extension.getURL('iframe.html');
+iframe.src = browser.extension.getURL('html/iframe.html');
 iframe.width = '0px';
 iframe.height = '0px';
 iframe.style.position = 'absolute';
