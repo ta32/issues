97c2a23d0a33a65389b1c67d8667271bd57c53d5
ta32 <26617779+ta32@users.noreply.github.com>
2023-03-02 21:02:43 +1100

    test solution

---
+++ ff_iframe/js/background.js
@@ -0,0 +1,16 @@
+console.log("test");
+// load iframe
+var iframe = document.createElement('iframe');
+// using remote src works
+// iframe.src = 'https://connect.trezor.io/8/iframe.html';
+iframe.src = browser.extension.getURL('iframe.html');
+iframe.width = '0px';
+iframe.height = '0px';
+iframe.style.position = 'absolute';
+iframe.style.display = 'none';
+iframe.style.border = '0px';
+iframe.style.width = '0px';
+iframe.style.height = '0px';
+iframe.id = 'trezorconnect';
+document.body.appendChild(iframe);
+console.log("src: " + iframe.src);
